47cbaf29d5a832182899b681cf5669f4289168fd
sgagnon <SGagnon@DimOnOff.com>
2021-10-23 22:40:20 -0400

    remove console logger override

--- app/Program.cs
+++ app/Program.cs
@@ -23,11 +23,6 @@ namespace app
 
         public static IHostBuilder CreateHostBuilder(string[] args) => 
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
